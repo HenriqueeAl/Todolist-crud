ca6e2cce3fc82a1539d2e264739f74b10cdcd300
henrique <henrique.almeida231@hotmail.com>
2022-12-01 14:30:02 -0300

    server try

--- server/db.ts
+++ server/db.ts
@@ -3,9 +3,9 @@ export const Sequelize = require('sequelize');
 require("dotenv").config();
 
 const sequelize = new Sequelize(process.env.BANCO, process.env.USER, process.env.PASSWORD,{
-    dialect: 'postgres',
-    host: 'localhost',
-    port: 5432 
+    dialect: process.env.DIALECT,
+    host: process.env.HOST,
+    port: process.env.PORT 
 })
 
 module.exports = sequelize;
--- server/index.ts
+++ server/index.ts
@@ -184,6 +184,6 @@ app.post('/consult', async (req: any, res:any)=>{
 
 //END - ROUTE CONSULT
 
-var server = http.createServer(app); 
-server.listen(5051);
-console.log("Servidor escutando na porta 5051...")
+const port = process.env.PORT || 3000
+
+app.listen(port , ()=>console.log("Servidor escutando na porta 5051..."));
